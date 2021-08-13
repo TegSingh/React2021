d28d46f8bf0165b21b3cbf15977ee7f621020515
TegSingh <tegveer2211@gmail.com>
2021-08-13 02:08:30 -0400

    Added tutorial for custom hooks

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,22 @@
 import { useState, useEffect } from 'react';
 
-export const useFetch = () => {};
+// Custom hooks need to be named as starting with 'use'
+export const useFetch = (url) => {
+    const [loading, setLoading] = useState(true)
+    const [products, setProducts] = useState([])
+
+    // Async await method [used as a replacement for ]
+    const getProducts = async () => {
+        const response = await fetch(url)
+        const products = await response.json()
+        setProducts(products)
+        setLoading(false)
+    };
+
+    // If this was not a custom hook, the dependecy array would've been empty byt the value of url can change now
+    useEffect(() => {
+        getProducts()
+    }, [url]);
+
+    return { loading, products }
+};
